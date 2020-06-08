7630ac3fb46363ce12c281e76b5071bec81890af
shakha <shislamove@gmail.com>
2020-06-08 17:33:01 +0200

    Initial Commit

---
+++ iResume/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iResume
+//
+//  Created by User on 6/8/20.
+//  Copyright © 2020 Shakhzod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
